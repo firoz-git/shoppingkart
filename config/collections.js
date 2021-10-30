2a060a655f4f80025ec37505453cda7abe919d0f
unknown <firoskhansha@gmail.com>
2021-10-30 19:52:14 +0530

    commmit

--- config/collections.js
+++ config/collections.js
@@ -1,3 +1,4 @@
 module.exports={
-    PRODUCT_COLLECTION:'product' //ith collection name an
+    PRODUCT_COLLECTION:'product', //ith collection name an
+    USER_COLLECTION:'user' //USER ENNA NAME ULLA ORU DATABASE COLLECTION (FOR USER DATA STORING)
 }
